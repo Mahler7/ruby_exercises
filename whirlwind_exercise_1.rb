690aa2a1d87d6b5306b0c974b8caef3b045fdaa7
Justin Jungst <jjungstprogramming@gmail.com>
2016-01-05 20:26:54 -0600

    Ruby Exercises

--- whirlwind_exercise_1.rb
+++ whirlwind_exercise_1.rb
@@ -1,12 +1,27 @@
+puts "What are your five favorite foods?"
+puts ""
 foods = []
 5.times do
-  puts "What are your five favorite foods?"
   foods << gets.chomp
 end
-p foods
 
 x = 0
 foods.each do |love|
   x = x + 1
   puts " #{x}. I love #{love}"
 end
+
+#Also works instead of counter aka x
+#foods.each_with_index |food, index|
+#  puts "#{index}. I love #{food}"
+#end
+
+#Also works
+#foods.length.times do |index|
+#  puts "{x}. I love {foods[x - 1]}"
+#end
+
+#Also works
+#foods.length.times do |index|
+#  puts "{index + 1}. #{food[index]}"
+#end
--- whirlwind_exercise_2.rb
+++ whirlwind_exercise_2.rb
@@ -1,9 +1,9 @@
-count = 0
-puts count
-10.times do
-  count = count + 1
-  puts count
-end
+# count = 0
+# puts count
+# 10.times do
+#   count = count + 1
+#   puts count
+# end
 
 sam_cook = 0
 sally_french = 0
@@ -18,9 +18,60 @@ while sam_and_sally_get_married == false
   end
 
   unless sam_cook > 12 and sally_french > 9
-    puts "Sam can crepes, Sally can speak French, now they can get married."
+    puts "Sam can make crepes, Sally can speak French, now they can get married."
     sam_and_sally_get_married = true
   end
+end
+
+#Lessons
+
+#if !(result == true) ... is the same as...
+#unless result == true
+
+# result = nil
+# unless result
+#   puts "Hello"
+# end 
+# -prints hello
+
+#-------Teachers solution---------
+
+#pseudo code
+# if sam can cook more than 10 recipes
+#   and sally speaksmore than 5 languages
+#   then they should date
+
+puts "Can Sam cook more than 10 recipes?"
+puts ""
+sam_can_cook = gets.chomp
+
+puts "Can Sally speak more than 5 languages?"
+puts ""
+sally_can_speak_well = gets.chomp
+
+if sam_can_cook == "yes" and sally_can_speak_well == "yes"
+  puts "Sam and Sally should date."
+  
+  puts "Can Sam make crepes?"
+  puts ""
+  sam_can_make_crepes = gets.chomp
+
+  puts "Can Sally speak French?"
+  puts ""
+  sally_speak_french = gets.chomp
+
+  if sam_can_make_crepes == "yes" or sally_speak_french == "yes"
+    puts "Then they should get married."
+  elsif sam_can_make_crepes == "no" or sally_speak_french == "no"
+    puts "Then they shouldn't rock the boat."
+  else
+    puts "Please answer with yes or no."
+  end
+
+elsif sam_Can_cook == "no" or sally_can_speak_well == "no"
+  puts "They should not date"
+else
+  puts "You need to answer with yes or no."
 end
 
 
