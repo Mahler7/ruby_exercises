e2afd8d5d1c60e8b674a3a20a0b59185f5e30a1b
Justin Jungst <jjungstprogramming@gmail.com>
2016-01-06 18:56:27 -0600

    ruby_exercises

--- whirlwind_exercise_4.rb
+++ whirlwind_exercise_4.rb
@@ -1,14 +1,72 @@
-classroom = []
-done = false
+#My try
+# classroom = []
+# odd_classroom = []
+# done = false
 
-puts "Enter a students name to pair them into a group."
-puts "Type 'done' when you are done."
-while done == false
-  name = gets.chomp
-  classroom << name
-  if name == "done"
-    done = true
-    classroom.reject{|done| done == "done"}
+# puts "Enter a students name to pair them into a group."
+# puts "Type 'done' when you are done."
+# while done == false
+#   name = gets.chomp
+#   classroom << name
+#   if name == "done"
+#     done = true
+#     classroom.pop
+#   end
+# end
+
+# if classroom.length % 2 == 0
+#   classroom.shuffle.each_slice(2) do |name_1, name_2|
+#     p [name_1, name_2]
+#   end
+# elsif classroom.length % 2 != 0
+#   classroom.shuffle.each_slice(2) do |name_1, name_2|
+#     odd_classroom << [name_1, name_2]
+#   end
+#   p odd_classroom[-1][-2 + -1]
+# end
+
+#----Teachers Solution ----
+
+#collect a list of names from the user into an array
+#mix up the array of names
+#group names together by 2's
+#odd number of names the last group = 3
+
+
+puts "Enter names of students and I'll make groups"
+puts ""
+
+names = []
+entered_name = ""
+
+until entered_name == "done"
+  unless entered_name == ""
+    names << entered_name
   end
+  entered_name = gets.chomp
 end
-p classroom
+
+mixed_up_names = names.shuffle
+
+until mixed_up_names.length == 0
+  
+  if mixed_up_names.length == 3
+    group = mixed_up_names.pop(3)
+  else
+    group = mixed_up_names.pop(2)
+  end
+
+  print "Group: "
+  group.each do |member|
+    print "#{member} "
+  end
+  puts ""
+end
+
+
+
+
+
+
+
+
